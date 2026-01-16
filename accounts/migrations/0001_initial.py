935bb9538fe23646ac644e569e002a7ad363c208
Jan Pobi <jpobi22@student.foi.hr>
2026-01-16 17:14:40 +0100

    migracija za profile
    migracija usera i pravila i jos audit log dodane funklcije i view i tablica te trigger

---
+++ accounts/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('metadata', models.JSONField(blank=True, default=dict)),
+                ('department', models.CharField(blank=True, max_length=120)),
+                ('job_title', models.CharField(blank=True, max_length=120)),
+                ('is_active_employee', models.BooleanField(default=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='profile', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
